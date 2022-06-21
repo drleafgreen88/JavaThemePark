e7dd1ae45015f8ea8c6ba1d1f92782e4a658641d
Derek Leafgreen <drl21788@gmail.com>
2022-06-21 09:12:49 +0100

    MVP done and extensions started

--- src/test/java/ThemeParkTest.java
+++ src/test/java/ThemeParkTest.java
@@ -1,2 +1,24 @@
+import attractions.*;
+import people.Visitor;
+import stalls.CandyflossStall;
+import stalls.IceCreamStall;
+import stalls.Stall;
+import stalls.TobaccoStall;
+
+import java.util.ArrayList;
+
 public class ThemeParkTest {
+
+    private ThemePark themePark;
+    private ArrayList <Attraction> attractions;
+    private ArrayList <Stall> stalls;
+    private Dodgems dodgems;
+    private Park park;
+    private Playground playground;
+    private RollerCoaster rollerCoaster;
+    private Visitor visitor;
+    private CandyflossStall candyflossStall;
+    private IceCreamStall iceCreamStall;
+    private TobaccoStall tobaccoStall;
+
 }
